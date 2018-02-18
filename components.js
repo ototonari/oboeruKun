0f79b904c2628f06ead99fcdfb68ae485b56aebd
Tsubasa <sinnyo_tsubasa@icloud.com>
2018-02-19 03:19:50 +0900

    edit components, fix configView

--- components.js
+++ components.js
@@ -14,9 +14,52 @@ export class Avatar extends Component {
           )
       } else {
           return (
-              <Image source={require('./images/camera.png')} />
+              <Image source={require('./assets/camera.png')} />
           )
       }
   }
 }
 
+export class FlatButton extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    style = StyleSheet.create({
+        container: {
+            borderColor: 'gray',
+            borderBottomWidth: 1,
+            height: 50,
+        },
+        inContainer: {
+            flex: 1,
+            flexDirection: 'row',
+            paddingLeft: 15,
+            paddingRight: 15,
+            alignItems: 'center',
+            justifyContent: 'space-between'
+        },
+        text: {
+            fontSize: 20
+        },
+        image: {
+            width: 20,
+            height: 20
+        }
+    })
+    render () {
+        const text = this.props.text
+        const func = this.props.function
+        return (
+            <TouchableOpacity 
+                onPress={() => func()}
+                style={this.style.container} >
+                    <View style={this.style.inContainer} >
+                        <Text style={this.style.text} >Developers</Text>
+                        <Image source={require('./assets/right.png')} style={this.style.image} />
+                    </View>
+            </TouchableOpacity>
+  
+        )
+    }
+}
--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Text, View, TouchableOpacity, Image, TextInput, Switch, Picker, Platform } from 'react-native';
 import styles from './configStyle'
 import { Actions } from "react-native-router-flux";
+import { FlatButton } from "../components";
 
 export default class ConfigView extends Component {
   constructor(props){
@@ -12,22 +13,8 @@ export default class ConfigView extends Component {
 
     return(
       <View style={{ flex: 1, }} >
-        <TouchableOpacity 
-          onPress={() => Actions.developers() }
-          style={{ 
-            borderColor: 'gray',
-            borderBottomWidth: 1,
-            borderTopWidth: 1,
-            height: 50,
-           }} >
-              <View style={{
-                flex: 1, flexDirection: 'row', paddingLeft: 15, paddingRight: 15, alignItems: 'center', justifyContent: 'space-between'
-              }} >
-                <Text style={{ fontSize: 20 }} >Developers</Text>
-                <Image source={require('../assets/right.png')} style={{ width: 20, height: 20 }} />
-              </View>
-        </TouchableOpacity>
-
+        <FlatButton text={'Developers'} function={() => Actions.developers()} />
+        
       </View>
     )
   }
